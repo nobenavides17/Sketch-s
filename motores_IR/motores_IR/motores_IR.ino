4a8ee6647652b06c8fb3e6b903b3ab5436bdc7c4
benavides <benavides@Benavides>
2016-05-25 20:38:52 -0600

    Prueba de motores con IR

---
+++ motores_IR/motores_IR/motores_IR.ino
@@ -0,0 +1,13 @@
+int p_motor1_1 = 6;
+int p_motor1_2 = 8;
+int p_motor2_1 = 10;
+int p_motor2_2 = 12;
+int p_ir = 4;
+void setup()
+{
+  pinMode(p_motor1_1, OUTPUT);
+  pinMode(p_motor1_2, OUTPUT);
+  pinMode(p_motor2_1, OUTPUT);
+  pinMode(p_motor2_2, OUTPUT);
+  pinMode(p_ir, INPUT);
+}
